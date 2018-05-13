639262cd2fcea98d6dbd3411b9e85f89e93d860a
Diogo Cezar <diogo@diogocezar.com>
2018-05-13 17:18:56 -0300

    Ajustes Gerais

--- tester.py
+++ tester.py
@@ -5,6 +5,14 @@ import sys
 import os.path
 import shutil
 from termcolor import colored
+import subprocess
+
+def run(cmd, logfile):
+    file = open(logfile,"w")
+    p = subprocess.Popen(cmd, shell=True, universal_newlines=True, stdout=file)
+    ret_code = p.wait()
+    file.flush()
+    return ret_code
 
 args = sys.argv
 extract = './tmp'
@@ -15,6 +23,8 @@ if len(args) < 2:
 
 file = args[1]
 (name,ext) = os.path.splitext(file)
+
+correction = args[2];
 
 if not os.path.isfile(file):
     print("O arquivo passado como parâmetro '{}' não existe.".format(file))
@@ -34,9 +44,10 @@ print("Recuperando arquivos de '{}'".format(location))
 
 for folder in listdir(location):
     if folder != '.DS_Store':
+        print("Analizando os arquivos de '{}'".format(location))
         locationInside = location + '/' + folder
-        print("Analizando os arquivos de '{}'".format(locationInside))
         source = locationInside + '/' + listdir(locationInside)[0]
+        print("Análise de similaridade para {}".format(locationInside))
         for folderCheck in listdir(location):
             if folderCheck != '.DS_Store' and folder != folderCheck:
                 locationCheck = location + '/' + folderCheck
@@ -44,7 +55,30 @@ for folder in listdir(location):
                 fileSource = open(source, "r").read()
                 fileSourceCheck = open(sourceCheck, "r").read()
                 equals = SequenceMatcher(None, fileSource, fileSourceCheck).ratio()
-                if equals > 0.6:
-                    print(colored("O arquivo {} é {:.2f}% parecido com o arquivo {}".format(source, equals, sourceCheck), 'red'))
+                if equals > 0.9:
+                    print(colored("   {} - {:.0f}% igual.".format(sourceCheck, (equals*100)), 'red'))
                 else:
-                    print(colored('OK', 'green'))
+                    print(colored("   {} - OK!".format(sourceCheck), 'green'))
+        print("Executando a compilação do programa.")
+        locationCompiled = locationInside + "/result.out"
+        executeCompiled = "./" + locationCompiled
+        outputCompiled = locationInside + "/result.txt"
+        print("Compilando o programa: '{}'".format(executeCompiled))
+        try:
+            subprocess.call(["gcc", source, "-o", locationCompiled])
+        except Exception as e:
+             print(colored("   {} - ERRO NA COMPILAÇÃO!".format(sourceCheck), 'red'))
+        print("Executando o programa e salvando a saída.")
+        output = run(executeCompiled, outputCompiled)
+        if correction:
+            print("Comparando a saída de dados com a correção.")
+            fileSourceOutput = open(outputCompiled, "r").read()
+            fileSourceOutputCheck = open(correction, "r").read()
+            equalsOutput = SequenceMatcher(None, fileSourceOutput, fileSourceOutputCheck).ratio()
+            if equalsOutput < 0.9:
+                print(colored("   {} - {:.0f}% igual a resposta.".format(fileSourceOutput, (equals*100)), 'red'))
+            else:
+                print(colored("   {} - RESPOSTA CORRETA!".format(correction), 'green'))
+
+print("Limpando a pasta temporária.")
+shutil.rmtree(extract, ignore_errors=True)
